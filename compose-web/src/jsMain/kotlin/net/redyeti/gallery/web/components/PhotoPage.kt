2673b4cd259f7c61415ffa8cc9d0d27c4ed44c7b
Chris Miller <chris.nz@gmail.com>
2023-04-11 21:31:54 +0100

    Display fullsized photos (badly...)

--- compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/components/PhotoPage.kt
+++ compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/components/PhotoPage.kt
@@ -1,19 +1,41 @@
 package net.redyeti.gallery.web.components
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import app.softwork.routingcompose.RouteBuilder
 import app.softwork.routingcompose.Routing
+import net.redyeti.gallery.remote.PopulatedAlbum
 import net.redyeti.gallery.repository.PhotoGalleryInterface
+import org.jetbrains.compose.web.dom.Img
+import org.jetbrains.compose.web.dom.Text
 
 @Routing
 @Composable
 fun RouteBuilder.PhotoPage(repo: PhotoGalleryInterface) {
+  var album: PopulatedAlbum? by remember { mutableStateOf(null) }
+  var photoId by remember { mutableStateOf(-1) }
+
   int { albumID ->
     int { photoID ->
-      LaunchedEffect(albumID) {
-        val album = repo.fetchAlbum(albumID)
+      if (albumID >= 0 && photoID >= 0) {
+        LaunchedEffect(albumID * 10000 + photoID) {
+          val popAlbum = repo.fetchAlbum(albumID)
+          if (popAlbum.photos.size > photoID) {
+            album = popAlbum
+            photoId = photoID
+          }
+        }
       }
     }
+  }
+
+  val popAlbum = album
+  if (popAlbum == null || photoId < 0) {
+    Text("Loading album...")
+  } else {
+    val photo = popAlbum.photos[photoId]
+    Img(
+      src = "/image/${popAlbum.album.directory}/large/${photo.filename}",
+      alt = photo.description
+    )
   }
 }
