80482ba689dc1b322af963fbf1c38daa2bc68425
chrismiller <chris.nz@gmail.com>
2023-05-16 19:25:22 +0100

    Improve the appearance of the fullscreen icons by changing their whiteness and transparency

--- compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/components/Lightbox.kt
+++ compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/components/Lightbox.kt
@@ -35,7 +35,3 @@ fun Lightbox(
     }
   }
 }
-
-fun sizedSVG(width: Int, height: Int): String {
-  return "data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 $width $height\"%3E%3C/svg%3E"
-}
