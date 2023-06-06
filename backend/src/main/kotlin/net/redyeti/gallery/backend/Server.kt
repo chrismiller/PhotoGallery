a5192cfba677da90545492dbf3458c30e99b53b4
chrismiller <chris.nz@gmail.com>
2023-06-06 21:45:16 +0100

    There's no need to specify the domain on the API URLs

--- backend/src/main/kotlin/net/redyeti/gallery/backend/Server.kt
+++ backend/src/main/kotlin/net/redyeti/gallery/backend/Server.kt
@@ -3,23 +3,15 @@ package net.redyeti.gallery.backend
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import net.redyeti.gallery.di.initKoin
-import net.redyeti.gallery.remote.PhotoGalleryApi
 
 
-private const val BASE_DIR = "F:/PhotoGallery"
 lateinit var appConfig: AppConfig
 lateinit var appData: AppData
 
 fun main() {
   System.setProperty("jdk.tls.client.protocols", "TLSv1.2")
 
-  val koin = initKoin(enableNetworkLogs = true).koin
-  val photoGalleryApi = koin.get<PhotoGalleryApi>()
-
   appConfig = ConfigLoader().load()
-
-  photoGalleryApi.baseUrl = "http://chrisdesktop:${appConfig.port}/api"
 
   appData = initGallery(appConfig)
 
