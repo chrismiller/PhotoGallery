c356ec59e70c4895a4267b227d2062022b30d08f
Chris Miller <chris.nz@gmail.com>
2023-06-14 00:00:31 +0100

    Move the fullscreen icon's opacity out of the SVG and into CSS, so we can change it on hover

--- compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/style/LightboxStyle.kt
+++ compose-web/src/jsMain/kotlin/net/redyeti/gallery/web/style/LightboxStyle.kt
@@ -103,15 +103,16 @@ object LightboxStyle : StyleSheet() {
     cursor("pointer")
     // backgroundImage("url(\"/fullscreen.svg\")")
     // fullscreenExit.svg after running through https://yoksel.github.io/url-encoder/
-    backgroundImage("url(\"data:image/svg+xml,%3Csvg width='40px' height='40px' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%23FFFFFF' opacity='0.5' d='M7 2H2v5l1.8-1.8L6.5 8 8 6.5 5.2 3.8 7 2zm6 0l1.8 1.8L12 6.5 13.5 8l2.7-2.7L18 7V2h-5zm.5 10L12 13.5l2.7 2.7L13 18h5v-5l-1.8 1.8-2.7-2.8zm-7 0l-2.7 2.7L2 13v5h5l-1.8-1.8L8 13.5 6.5 12z'/%3E%3C/g%3E%3C/svg%3E")
+    backgroundImage("url(\"data:image/svg+xml,%3Csvg width='40px' height='40px' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%23FFFFFF' d='M7 2H2v5l1.8-1.8L6.5 8 8 6.5 5.2 3.8 7 2zm6 0l1.8 1.8L12 6.5 13.5 8l2.7-2.7L18 7V2h-5zm.5 10L12 13.5l2.7 2.7L13 18h5v-5l-1.8 1.8-2.7-2.8zm-7 0l-2.7 2.7L2 13v5h5l-1.8-1.8L8 13.5 6.5 12z'/%3E%3C/g%3E%3C/svg%3E")
     position(Position.Absolute)
     right(0.px)
     bottom(40.px)
     width(40.px)
     height(40.px)
+    opacity(0.5)
     property("z-index", LightboxVars.zIndexBase + 6)
     self + hover style {
-      color(rgb(1, 1, 0))
+      opacity(0.8)
     }
   }
 
@@ -119,7 +120,7 @@ object LightboxStyle : StyleSheet() {
     // :fullscreen .fullscreen { ... }
     desc(":fullscreen", className(fullscreen)) style {
       // fullscreenExit.svg after running through https://yoksel.github.io/url-encoder/
-      backgroundImage("url(\"data:image/svg+xml,%3Csvg width='40px' height='40px' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%23FFFFFF' opacity='0.5' d='M3.4 2L2 3.4l2.8 2.8L3 8h5V3L6.2 4.8 3.4 2zm11.8 4.2L18 3.4 16.6 2l-2.8 2.8L12 3v5h5l-1.8-1.8zM4.8 13.8L2 16.6 3.4 18l2.8-2.8L8 17v-5H3l1.8 1.8zM17 12h-5v5l1.8-1.8 2.8 2.8 1.4-1.4-2.8-2.8L17 12z'/%3E%3C/g%3E%3C/svg%3E")
+      backgroundImage("url(\"data:image/svg+xml,%3Csvg width='40px' height='40px' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%23FFFFFF' d='M3.4 2L2 3.4l2.8 2.8L3 8h5V3L6.2 4.8 3.4 2zm11.8 4.2L18 3.4 16.6 2l-2.8 2.8L12 3v5h5l-1.8-1.8zM4.8 13.8L2 16.6 3.4 18l2.8-2.8L8 17v-5H3l1.8 1.8zM17 12h-5v5l1.8-1.8 2.8 2.8 1.4-1.4-2.8-2.8L17 12z'/%3E%3C/g%3E%3C/svg%3E")
       bottom(0.px)
       position(Position.Fixed)
     }
